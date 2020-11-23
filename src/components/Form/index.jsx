74fea200640d67b640badd146909d3a26352f29b
Raul Franck <raulengfranck@gmail.com>
2020-11-22 23:58:59 -0300

    estilização final

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -37,7 +37,7 @@ const Form = () => {
         <form>
           <input
             type="text"
-            placeholder="Procurar"
+            placeholder="Name"
             value={username}
             onChange={onChangeHandler}
           />
